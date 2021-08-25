4948cea681c2fa8e23bb0cd9390dc4a4d31e66a0
user.email <rafael.xavier2504@gmail.com>
2021-08-25 11:18:54 -0300

    Atividade aula JavaScript

---
+++ aula05/script.js
@@ -0,0 +1,49 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano') 
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        alert('[Erro] Verifique os dados e tente novamente!')
+    }else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked){
+            genero = 'Homem'            
+            if (idade >= 0 && idade < 10){
+                //Criança
+                img.setAttribute('src','bebeHomem.jpg')
+            } else if (idade < 21){
+                //Jovem
+                img.setAttribute('src', 'jovemHomem.jpg')
+            } else if (idade < 50){
+                //Adulto
+                img.setAttribute('src', 'homemAdulto.jpg')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'idosoHomem.jpg')
+            }
+        } else if (fsex[1].checked){ 
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 10){
+                //Criança
+                img.setAttribute('src', 'bebeMulher.jpg')
+            } else if (idade < 21){
+                //Jovem
+                img.setAttribute('src', 'jovemMulher.jpg')
+            } else if (idade < 50){
+                //Adulto
+                img.setAttribute('src', 'adultoMulher.jpg')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'idosoMulher.jpg')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
